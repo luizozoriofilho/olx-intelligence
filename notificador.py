953c028bcbeddca4de57af3dfae7b0052bee1449
luizozoriofilho <luizozoriofilho@gmail.com>
2026-03-17 23:27:50 -0300

    Update notificador.py

--- notificador.py
+++ notificador.py
@@ -4,5 +4,5 @@ TOKEN = "SEU_TOKEN_AQUI"
 CHAT_ID = "SEU_CHAT_ID_AQUI"
 
 def enviar_telegram(mensagem):
-    url = f"https://api.telegram.org/bot8786728156:AAGvSmgSkjI2o_rid4R1NjIiychJaNjn6gE/sendMessage"
+    url = f"https://api.telegram.org/botCODIGO_BOT:CODIGO_API/sendMessage"
     requests.post(url, data={"chat_id": CHAT_ID, "text": mensagem})
