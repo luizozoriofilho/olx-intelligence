101c47f686c3ef24124c597326f17630c666c3e4
luizozoriofilho <luizozoriofilho@gmail.com>
2026-03-17 23:30:57 -0300

    Delete notificador.py

--- notificador.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-TOKEN = "SEU_TOKEN_AQUI"
-CHAT_ID = "SEU_CHAT_ID_AQUI"
-
-def enviar_telegram(mensagem):
-    url = f"https://api.telegram.org/botCODIGO_BOT:CODIGO_API/sendMessage"
-    requests.post(url, data={"chat_id": CHAT_ID, "text": mensagem})
